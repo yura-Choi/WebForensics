8e626b7756ac1be1f7d3bf78ae712c2d9c026a80
dodogcat <ttttwww10@gmail.com>
2021-11-26 01:42:53 +0900

    jtable 버전

--- webforensic/gui/Dialog.java
+++ webforensic/gui/Dialog.java
@@ -6,6 +6,7 @@ import downloads.DownloadsTableModel;
 import urls.UrlsTableModel;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,6 +20,7 @@ public class Dialog extends JDialog {
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         int row;
+        String[] header = {"type","data"};
         String[] names = null;
         String[] searched_detail = null;
 
@@ -43,27 +45,51 @@ public class Dialog extends JDialog {
             searched_detail = CacheTableModel.getInstance().detail(input[2], input[3]);
         }
 
-        for(int i=0;i<row;i++){
-            gbc.gridx = 0;
-            gbc.gridy = i;
-            gbc.gridwidth = 1;
-            gbc.gridheight = 1;
-            gbc.fill = GridBagConstraints.VERTICAL;
-            gbc.weightx = 2;
+//        for(int i=0;i<row;i++){
+//            gbc.gridx = 0;
+//            gbc.gridy = i;
+//            gbc.gridwidth = 1;
+//            gbc.gridheight = 1;
+//            gbc.fill = GridBagConstraints.VERTICAL;
+//            gbc.weightx = 2;
+//
+//            add(new JLabel(names[i]), gbc);
+//            gbc.gridx = 1;
+//            gbc.gridy = i;
+//            gbc.gridwidth = 1;
+//            gbc.gridheight = 1;;
+//            gbc.fill = GridBagConstraints.VERTICAL;
+//            gbc.weightx = 8;
+//
+//            add(new JLabel(searched_detail[i]), gbc);
+//        }
 
-            add(new JLabel(names[i]), gbc);
-            gbc.gridx = 1;
-            gbc.gridy = i;
-            gbc.gridwidth = 1;
-            gbc.gridheight = 1;;
-            gbc.fill = GridBagConstraints.VERTICAL;
-            gbc.weightx = 8;
+        String[][] contents = new String[row][2];
 
-            add(new JLabel(searched_detail[i]), gbc);
+        for(int i=0; i<row;i++){
+            contents[i][0] = names[i];
+            contents[i][1] = searched_detail[i];
         }
+
+        gbc.fill = GridBagConstraints.BOTH;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+
+        DefaultTableModel dtm = new DefaultTableModel(contents,header);
+
+        JTable j = new JTable(dtm);
+        JScrollPane sp = new JScrollPane(j);
+//        JList rowheader = new JList()
+//        sp.setRowHeaderView(j.getRowHeight());
+
+        add(sp, gbc);
 
 //        add(tf);
 //        add(okButton);
+
         setSize(700,500);
         setVisible(true);
 
